0fa16fa2a41494e44821d9bf6fb17f97131af589
ktaku1229 <ktaku1229@gmail.com>
2017-04-26 14:45:01 +0900

    W06_task1 added

---
+++ W06/main.js
@@ -0,0 +1,84 @@
+function main()
+{
+    var width = 500;
+    var height = 500;
+
+    var scene = new THREE.Scene();
+
+    var fov = 45;
+    var aspect = width / height;
+    var near = 1;
+    var far = 1000;
+    var camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
+    camera.position.set( 0, 0, 5 );
+    scene.add( camera );
+
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize( width, height );
+    document.body.appendChild( renderer.domElement );
+
+    var vertices = [
+	[-1, 1, 1],
+	[-1, -1, 1],
+	[1, -1, 1],
+	[1, 1, 1],
+	[-1, 1, -1],
+	[-1, -1, -1],
+	[1, -1, -1],
+	[1, 1, -1]
+    ];
+
+    var faces = [
+	[0, 1, 2],
+	[3, 0, 2],
+	[1, 5, 6],
+	[2, 1, 6],
+	[2, 6, 7],
+	[3, 2, 7],
+	[3, 7, 4],
+	[0, 3, 4],
+	[0, 4, 5],
+	[1, 0, 5],
+	[4, 6, 5],
+	[4, 7, 6]
+    ];
+    
+    var v = [];
+    for (var i=0; i<8; i++){
+	v[i] = new THREE.Vector3().fromArray(vertices[i]);
+    }
+    
+    var f = [];
+    var id;
+    for(var i=0; i<12; i++){
+	id = faces[i];
+	f[i] = new THREE.Face3(id[0], id[1], id[2]);
+    }
+    
+    var geometry = new THREE.Geometry();
+    for(var i=0; i<8; i++){
+	geometry.vertices.push(v[i]);
+    }
+    for(var i=0; i<12; i++){
+	geometry.faces.push(f[i]);
+    }
+ 
+    var material = new THREE.MeshBasicMaterial();
+    material.vertexColors = THREE.FaceColors;
+    for(var i=0; i<12; i++){
+	geometry.faces[i].color = new THREE.Color(1,1,1);
+    }
+    
+    var cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
+
+    loop();
+
+    function loop()
+    {
+        requestAnimationFrame( loop );
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+        renderer.render( scene, camera );
+    }
+}
